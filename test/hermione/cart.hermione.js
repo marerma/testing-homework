90180be5002486a1d4e584fc90f2196da67c61ed
marerma <maria.kadantseva@gmail.com>
2023-07-01 16:07:51 +0300

    feat: add form validation test

--- test/hermione/cart.hermione.js
+++ test/hermione/cart.hermione.js
@@ -34,4 +34,26 @@ describe('Корзина', async function() {
       const totalPriceAfterReload = await browser.$('.Cart-OrderPrice').getText();
       await expect(totalPriceBeforeReload).toEqual(totalPriceAfterReload)
     });
+
+
+    it('после заполнения формы, появляется сообщение об успешной отправке заказа', async function({browser}) {
+      const puppeteer = await browser.getPuppeteer();
+      const [page] = await puppeteer.pages();
+      await browser.setWindowSize(1920, 1080);
+      await page.goto(`${ROUTES.catalog}/1`);
+      const addBtn1 = await browser.$('.ProductDetails-AddToCart');
+      await addBtn1.click();
+
+      await page.goto(ROUTES.cart);
+      await browser.$('#f-name').setValue('Иван Иванович');
+      await browser.$('#f-phone').setValue('8912345678');
+      await browser.$('#f-address').setValue('Moscow');
+      await browser.$('.Form-Submit').click();
+
+      await expect(await browser.$('.alert-heading').getText()).toEqual('Well done!');
+
+      await browser.assertView('formsuccess', '.Cart-SuccessMessage', {
+        ignoreElements: ['.Cart-Number']
+      });
+    });
 })
---
+++ test/unit/form.test.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { renderWithProviders } from "./helpers";
+import { Form } from '../../src/client/components/Form';
+import {screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { CheckoutFormData } from "../../src/common/types";
+
+describe('Форма на странице корзины', () => {
+  it('при вводе корректных данных срабатывает отправка данных', async () => {
+    const user = userEvent.setup()
+    const userData = {
+      name: 'Иван Иванов',
+      phone: '8911111111',
+      address: 'Москва',
+  }
+    const onSubmit = jest.fn((data: CheckoutFormData)=> data)  
+    renderWithProviders(<Form onSubmit={onSubmit} />)
+    const nameInput = screen.getByTestId('f-name')
+    const phoneInput = screen.getByTestId('f-phone')
+    const addressInput = screen.getByTestId('f-address')
+    const submitBtn = screen.getByRole('button', {name: 'Checkout'})
+
+    await user.type(nameInput, userData.name);
+    await user.type(addressInput, userData.address);
+    await user.type(phoneInput, userData.phone);
+
+    expect(nameInput).toHaveValue(userData.name)
+    expect(phoneInput).toHaveValue(userData.phone)
+    expect(addressInput).toHaveValue(userData.address)
+    await user.click(submitBtn);
+
+    expect(onSubmit).toHaveBeenCalledTimes(1) 
+    expect(onSubmit).toHaveBeenCalledWith( {
+      name: userData.name,
+      phone: userData.phone,
+      address: userData.address,
+    });
+  });
+  it('при вводе некорректного телефона появляется ошибка', async () => {
+    const user = userEvent.setup()
+    const userData = {
+      name: 'Иван Иванов',
+      phone: '891111111',
+      address: 'Москва',
+  }
+    const onSubmit = jest.fn((data: CheckoutFormData)=> data)  
+    renderWithProviders(<Form onSubmit={onSubmit} />)
+    const phoneInput = screen.getByTestId('f-phone')
+    const submitBtn = screen.getByRole('button', {name: 'Checkout'})
+
+    await user.type(phoneInput, userData.phone);
+
+    expect(phoneInput).toHaveValue(userData.phone)
+    await user.click(submitBtn);
+
+    expect(onSubmit).not.toBeCalled() 
+    expect(screen.getByText('Please provide a valid phone')).toBeVisible();
+  });
+  it('если не ввести имя, появляется ошибка', async () => {
+    const user = userEvent.setup();
+
+    const onSubmit = jest.fn((data: CheckoutFormData)=> data)  
+    renderWithProviders(<Form onSubmit={onSubmit} />);
+    const nameInput = screen.getByTestId('f-name');
+    expect(nameInput).toHaveValue('');
+    const submitBtn = screen.getByRole('button', {name: 'Checkout'});
+    
+    await user.click(submitBtn);
+
+    expect(onSubmit).not.toBeCalled() 
+    expect(screen.getByText('Please provide your name')).toBeVisible();
+  });
+  it('если не ввести имя, появляется ошибка', async () => {
+    const user = userEvent.setup();
+
+    const onSubmit = jest.fn((data: CheckoutFormData)=> data)  
+    renderWithProviders(<Form onSubmit={onSubmit} />);
+    const addressInput = screen.getByTestId('f-address')
+    expect(addressInput).toHaveValue('');
+    const submitBtn = screen.getByRole('button', {name: 'Checkout'});
+    
+    await user.click(submitBtn);
+
+    expect(onSubmit).not.toBeCalled() 
+    expect(screen.getByText('Please provide a valid address')).toBeVisible();
+  });
+})
